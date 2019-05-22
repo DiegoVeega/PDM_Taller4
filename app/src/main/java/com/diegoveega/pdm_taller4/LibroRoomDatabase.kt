f82e1de5f5271ca94c37298f72c7ebcd3a140877
DiegoVeega <00148816@uca.edu.sv>
2019-05-21 19:23:48 -0600

    Rellenar base de datos

--- app/src/main/java/com/diegoveega/pdm_taller4/LibroRoomDatabase.kt
+++ app/src/main/java/com/diegoveega/pdm_taller4/LibroRoomDatabase.kt
@@ -4,11 +4,15 @@ import android.content.Context
 import androidx.room.Database
 import androidx.room.Room
 import androidx.room.RoomDatabase
+import androidx.sqlite.db.SupportSQLiteDatabase
 import com.diegoveega.pdm_taller4.dao.AutorDao
 import com.diegoveega.pdm_taller4.dao.LibroDao
 import com.diegoveega.pdm_taller4.dao.TAGDao
 import com.diegoveega.pdm_taller4.models.Autor
 import com.diegoveega.pdm_taller4.models.Libro
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
 
 @Database(entities = [Libro::class], version = 1 ,exportSchema = false)
 abstract class LibroRoomDatabase : RoomDatabase() {
@@ -21,19 +25,28 @@ abstract class LibroRoomDatabase : RoomDatabase() {
         @Volatile
         private var INSTANCE: LibroRoomDatabase? = null
 
-        fun getDatabase(context: Context): LibroRoomDatabase {
-            val tempInstance = INSTANCE
-            if (tempInstance != null) {
-                return tempInstance
-            }
-            synchronized(this) {
-                val instance = Room.databaseBuilder(
-                    context.applicationContext,
-                    LibroRoomDatabase::class.java,
-                    "Libro_database"
-                ).build()
+        fun getDatabase(context: Context, scope: CoroutineScope): LibroRoomDatabase {
+
+            return INSTANCE ?: synchronized(this){
+                val instance = Room.databaseBuilder(context.applicationContext,LibroRoomDatabase::class.java,"libro_database")
+                    .fallbackToDestructiveMigration()
+                    .addCallback(LibroDatabaseCallback(scope))
+                    .build()
                 INSTANCE = instance
-                return instance
+                instance
+            }
+        }
+
+        private class LibroDatabaseCallback(private val scope: CoroutineScope): RoomDatabase.Callback(){
+            override fun onOpen(db: SupportSQLiteDatabase) {
+                super.onOpen(db)
+                // If you want to keep the data through app restarts,
+                // comment out the following line.
+                INSTANCE?.let { database ->
+                    scope.launch(Dispatchers.IO) {
+                        populateDatabase(database.libroDao())
+                    }
+                }
             }
         }
     }
